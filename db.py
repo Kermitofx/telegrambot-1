1cb5a00e9a79aa389f02ee9ce0e783104ec971da
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-01-11 19:41:03 -0300

    Update db.py

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ class SearchData(Base):
     id = Column(Integer, primary_key=True)
     song_short=Column(String)
     song_link=Column(String)
-engine=create_engine("DATABASE_URL")
+engine=create_engine("postgres://bniufsqeyqiebh:be2d82048281446cad3890a5e5b02f6920be7e718f0a130b083bae68e9eab694@ec2-174-129-33-107.compute-1.amazonaws.com:5432/d17fc4io4po963")
 
 def loadSession():
     Session = sessionmaker(bind=engine)
